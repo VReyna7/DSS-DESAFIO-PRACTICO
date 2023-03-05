f9a60d0b8aa481e9939a53fe59275de1faf8b329
Juan <89045210+VReyna7@users.noreply.github.com>
2023-03-04 20:21:01 -0600

    Add files via upload

--- modelo/class.validaciones.php
+++ modelo/class.validaciones.php
@@ -2,7 +2,7 @@
 class Validacion{
 
     public function valCod($cod){
-        return preg_match("^(PROD)[\d]{4}$", $cod);
+        return preg_match("/^(PROD)[\d]{4}$/", $cod);
     }
 
     public function isVoid($val){
