cb5d58eb4e527d6b38e381576521abab83a4ca19
Juan <89045210+VReyna7@users.noreply.github.com>
2023-03-04 20:20:42 -0600

    Add files via upload

--- vista/crearProducto.php
+++ vista/crearProducto.php
@@ -6,16 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Crear Producto</title>
+    <link rel="stylesheet" href="../css/formu.css?v=<?php echo time()?>">
 </head>
 <body>
+    <nav class="navbar navbar-dark bg-warning titulo-nav">
+        <a class="navbar-brand text-dark" href="#">TextilExport</a>
+        <a href="admin.php"><button class="btn btn-danger">Regresar</button></a>
+    </nav>
     <main>
+    <h1>Crear Producto</h1>
         <div class="container">
         <div class="error_log">
             <?php
             if(isset($_GET['error_log']) && $_GET['error_log'] == "1"){
                 echo "<p class=\"alert alert-danger\">Complete los campos requeridos</p>" ;
             }elseif(isset($_GET['error_log']) && $_GET['error_log'] == "2"){
-                echo "<p class=\"alert alert-danger\">El codigo no tiene el formato correcto</p>" ;
+                echo "<p class=\"alert alert-danger\">El codigo no tiene el formato correcto</p>";
+            }elseif(isset($_GET['error_log']) && $_GET['error_log'] == "3"){
+                echo "<p class=\"alert alert-danger\">Precio o existencia no puede estar vacio o ser un negativo</p>" ;
             }
             ?>
         </div>
@@ -38,18 +46,18 @@
             </div>
             <div class="mb-3">
                 <label for="" class="form-label">Digite la categoria:</label>
-                <select name="categoria" id="">
+                <select name="categoria" id="" >
                     <option value="Textil">Textil</option>
                     <option value="Promocional">Promocional</option>
                 </select>
             </div>
             <div class="mb-3">
                 <label for="" class="form-label">Digite el precio:</label>
-                <input type="text" class="form-control" name="precio" >
+                <input type="number" class="form-control" name="precio">
             </div>
             <div class="mb-3">
                 <label for="" class="form-label">Digite las existencias:</label>
-                <input type="text" class="form-control" name="existencias" >
+                <input type="number" class="form-control" name="existencias">
             </div>
             <div class="mb-3">
                 <input type="submit" class="btn btn-primary"  >
@@ -57,5 +65,13 @@
         </form>
         </div>
     </main> 
+
+    <footer class="bg-warning text-center text-lg-start footer">
+        <!-- Copyright -->
+        <div class="text-center text-dark p-3">
+        <p>Cristian Pineda PC222718 / Víctor Reyna RM222722</p>
+        </div>
+        <!-- Copyright -->
+      </footer>
 </body>
 </html>
--- vista/modificarProducto.php
+++ vista/modificarProducto.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Crear Producto</title>
+    <link rel="stylesheet" href="../css/formu.css?v=<?php echo time()?>">
     <?php
     include("../modelo/class.producto.php");
     $pro = new Producto();
@@ -13,6 +14,10 @@
     ?>
 </head>
 <body>
+<nav class="navbar navbar-dark bg-warning titulo-nav">
+        <a class="navbar-brand text-dark" href="#">TextilExport</a>
+        <a href="admin.php"><button class="btn btn-danger">Regresar</button></a>
+    </nav>
     <main>
         <h1>Modificar Producto</h1>
         <div class="container">
@@ -20,6 +25,8 @@
             <?php
             if(isset($_GET['error_log']) && $_GET['error_log'] == "1"){
                 echo "<p class=\"alert alert-danger\">Complete los campos requeridos</p>" ;
+            }elseif(isset($_GET['error_log']) && $_GET['error_log'] == "2"){
+                echo "<p class=\"alert alert-danger\">El precio y exitencias no puede estar vacio o negativo</p>";
             }
             ?>
         </div>
@@ -58,11 +65,11 @@
             </div>
             <div class="mb-3">
                 <label for="" class="form-label">Digite el precio:</label>
-                <input type="text" class="form-control" name="precio" value="<?= $pro->getPrecio()?>">
+                <input type="number" class="form-control" name="precio" value="<?= $pro->getPrecio()?>">
             </div>
             <div class="mb-3">
                 <label for="" class="form-label">Digite las existencias:</label>
-                <input type="text" class="form-control" name="existencias" value="<?= $pro->getExistencias()?>">
+                <input type="number" class="form-control" name="existencias" value="<?= $pro->getExistencias()?>">
             </div>
             <div class="mb-3">
                 <input type="submit" class="btn btn-primary" >
@@ -70,5 +77,13 @@
         </form>
         </div>
     </main> 
+
+    <footer class="bg-warning text-center text-lg-start footer">
+        <!-- Copyright -->
+        <div class="text-center text-dark p-3">
+        <p>Cristian Pineda PC222718 / Víctor Reyna RM222722</p>
+        </div>
+        <!-- Copyright -->
+      </footer>
 </body>
 </html>
